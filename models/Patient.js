3150f49c7c728d206f9d6183fd08e3cccdc22960
Ajay Wagh <54326598+Ajaywagh2298@users.noreply.github.com>
2024-09-12 15:20:05 +0530

    Update Patient.js

--- models/Patient.js
+++ models/Patient.js
@@ -4,12 +4,12 @@ const PatientSchema = new mongoose.Schema({
     _id: { type: String },
     uid: { type: String, required: true, unique: true },
     name: { type: String, required: true },
-    dateOfBirth: { type: Date, required: true },
-    weight: { type: Number, required: true },
-    height: { type: Number, required: true },
-    bloodGroup: { type: String, required: true },
-    address: { type: String, required: true },
-    healthStatus: { type: String, required: true },
+    dateOfBirth: { type: String },
+    weight: { type: String },
+    height: { type: String },
+    bloodGroup: { type: String },
+    address: { type: String  },
+    healthStatus: { type: String },
     staffUid : { type: String }
 });
 
